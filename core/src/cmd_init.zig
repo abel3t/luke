54c99e4aacf969fa3e818d95e7edd6001f26d198
Abel Tran <abeltran.contact@gmail.com>
2026-08-12 18:22:50 +0700

    fix init cmd

--- core/src/cmd_init.zig
+++ core/src/cmd_init.zig
@@ -72,6 +72,9 @@ pub fn run(allocator: std.mem.Allocator, io: std.Io, workspace_path: []const u8,
 }
 
 fn runSingleFolder(allocator: std.mem.Allocator, io: std.Io, workspace_path: []const u8, home_dir: []const u8) !void {
+    var stderr_buf: [512]u8 = undefined;
+    var err = std.Io.File.Writer.init(std.Io.File.stderr(), io, &stderr_buf);
+
     std.debug.print("Luke Engine: Scanning Workspace: {s}...\n\n", .{workspace_path});
 
     var ast_engine = try engine.AstEngine.init(allocator, io);
@@ -94,6 +97,16 @@ fn runSingleFolder(allocator: std.mem.Allocator, io: std.Io, workspace_path: []c
         break :blk try std.fs.path.join(allocator, &[_][]const u8{ cwd, workspace_path });
     };
     defer allocator.free(absolute_workspace_path);
+
+    var check_dir = std.Io.Dir.openDirAbsolute(io, absolute_workspace_path, .{}) catch {
+        try err.interface.print(
+            "Luke init failed: workspace path not found: {s}\n\nTry from a repo root with:\n  luke init .\n\nOr pass an absolute path:\n  luke init /path/to/repo\n",
+            .{absolute_workspace_path},
+        );
+        try err.flush();
+        return;
+    };
+    check_dir.close(io);
 
     var ws_walker = try walker_mod.WorkspaceWalker.init(allocator, io, absolute_workspace_path);
     defer ws_walker.deinit();
